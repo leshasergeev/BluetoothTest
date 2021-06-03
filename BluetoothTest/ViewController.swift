19bd1fd5c566f2d67c7f3056ef0b44a3e95555c1
iBamboola <41113234+iBamboola@users.noreply.github.com>
2021-06-03 12:25:56 +0300

    Initial Commit

---
+++ BluetoothTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BluetoothTest
+//
+//  Created by Алексей Сергеев on 03.06.2021.
+//  Copyright © 2021 Алексей Сергеев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
